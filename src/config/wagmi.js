272ad82479ffaaa14d431d266878fbe4d981209a
Dinh Nguyen Duc Tam <23521384@gm.uit.edu.vn>
2026-03-25 20:39:13 +0700

    branch bug

--- src/config/wagmi.js
+++
@@ -1,9 +0,0 @@
-import { http, createConfig } from 'wagmi'
-import { sepolia } from 'wagmi/chains'
-
-export const config = createConfig({
-  chains: [sepolia],
-  transports: {
-    [sepolia.id]: http(),
-  },
-})
