9e995f8d0a80e4026b2cc9914c334e7d3c2da2bf
thinh1311ss <pthinhn.com>
2026-04-26 11:31:56 +0700

    fix: case sensitivity AuthMiddleware in all routes

--- server/Router/NFTRoute.js
+++ server/Router/NFTRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const nftController = require("../Controllers/NFTController");
-const authMiddleware = require("../Middleware/authMiddleware");
+const authMiddleware = require("../Middleware/AuthMiddleware");
 
 router.get("/myNFTs", authMiddleware.isAuthentication, nftController.getMyNFTs);
 
